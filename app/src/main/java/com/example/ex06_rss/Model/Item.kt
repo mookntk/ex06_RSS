fa7835978203e86d7e4f3bb3bc4e28c20d6090de
mook_ntk <mook.nattakan309@gmail.com>
2019-09-27 21:57:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ex06_rss/Model/Item.kt
@@ -0,0 +1,3 @@
+package com.example.ex06_rss.Model
+
+data class Item( val title:String, val pubDate:String, val link:String, val guid:String, val author:String, val thumbnail:String, val description:String, val content:String, val enclouser:Object, val categories:List<String>)
